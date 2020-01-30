b06199795162ef2a0589b2b1ddee24cb8d2a43b6
Ge Sha At u18vmHP8570w <gesha@uniposta.de>
2020-01-30 09:47:15 -0800

    ADD: pring data

--- src/DNSTunnelServer.java
+++ src/DNSTunnelServer.java
@@ -260,7 +260,7 @@ public class DNSTunnelServer implements Runnable, DNSTunnelConstants {
         debug("processData(): decoded data = " + data);
         int pos = data.indexOf(SEPARATOR);
         if (pos < 0) {
-            throw new IOException("Wrong request, couldn't find a separator");
+            throw new IOException("Wrong request, couldn't find a separator::"+data+"::");
         }
         String output = data.substring(pos + 1);
         if (!output.isEmpty()) {
